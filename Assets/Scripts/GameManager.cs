a2c661a7184a8fb1352cf11ad2efbcb30c65bd02
Connor Leib <cjl35@pct.edu>
2024-09-17 10:46:45 -0400

    Basic GameManager Script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
+    public int health = 100;
+    public int ammo = 5;
+    public int coins = 0;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
     }
 }
