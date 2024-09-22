08cfdf73c4e8232659d2ce1409cd0e76c4486956
CAKECHALICE <cak44@pct.edu>
2024-09-22 10:39:20 -0400

    Platform
    The platform works but the raycast needs to be adjusted down

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,10 +12,18 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rbody;
     private float delay = .8f;
     private bool isJumping = false;
+
+    private TilemapCollider2D tilemapCollider;
     // Start is called before the first frame update
+
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        GameObject tilemapObject = GameObject.Find("Tilemap_Dropdown_Platform");
+        if (tilemapObject != null) 
+        {
+            tilemapCollider = tilemapObject.GetComponent<TilemapCollider2D>();
+        }
     }
 
     // Update is called once per frame
@@ -33,25 +42,28 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.W) && !isJumping)
             StartCoroutine(JumpPeriod());
-        void FlipX()
-        {
-            Vector3 theScale = transform.localScale;
-            theScale.x = theScale.x * -1;
-            transform.localScale = theScale;
-        }
     }
 
     private void FixedUpdate()
     {
+        LayerMask PlayerMask = LayerMask.GetMask("playerLayer");
+
         if (rbody.velocity.x < 10 && rbody.velocity.x > -10)
             rbody.AddForce(Vector2.right * Mathf.Round(H) * moveForce);
 
         //if (rbody.velocity.y < 2 && rbody.velocity.y > -2) // dropdown platform
-        //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
-        //if (hit.collider.CompareTag("platform")
-        //{
-            
-        //}
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, ~PlayerMask);
+        Debug.DrawRay(transform.position, Vector2.down, Color.red);
+        Debug.Log(hit.collider);
+
+        if (hit.collider != null && hit.collider.CompareTag("Platform"))
+        {
+            tilemapCollider.isTrigger = false;
+        }
+        else
+        {
+            tilemapCollider.isTrigger = true;
+        }
 
     }
     IEnumerator JumpPeriod()
@@ -63,5 +75,12 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(delay);
         
         isJumping = false;
+    }
+
+    void FlipX()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x = theScale.x * -1;
+        transform.localScale = theScale;
     }
 }
