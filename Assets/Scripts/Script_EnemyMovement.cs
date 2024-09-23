40daa75bfdb3b5a3bfd508936ae4be67e471d89f
CAKECHALICE <cak44@pct.edu>
2024-09-23 11:21:33 -0400

    school computer

--- Assets/Scripts/Script_EnemyMovement.cs
+++ Assets/Scripts/Script_EnemyMovement.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 
 public class Script_EnemyMovement : MonoBehaviour
 {
+    public enum EnemyType { Melee, Ranged }
+    public EnemyType enemyType;
+
     public Vector2 facingDirection = Vector2.right;
     public float moveForce;
-    private Rigidbody2D rbody;
-    public GameObject bullet_prefab;
-    private Vector2 switchX = Vector2.left;
     public float moveSpeed;
-    // Start is called before the first frame update
+
+    // for ranged enemy
+    public GameObject bullet_prefab;
+
+    private Vector2 switchX = Vector2.left;
+    private Rigidbody2D rbody;
+
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
@@ -21,15 +27,15 @@ public class Script_EnemyMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Enemies are put on their own layer so then the raycast can ignore them with ~EnemyMask
         LayerMask EnemyMask = LayerMask.GetMask("enemyLayer");
-        
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, ~EnemyMask);
+
         //Debug.DrawRay(transform.position, Vector2.down, Color.red);
         //Debug.Log(hit.collider);
         
-        if (hit.collider == null)
+        if (hit.collider == null && rbody.velocity.y == 0)
         {
-
             if (switchX == Vector2.left)
             {
                 switchX = Vector2.right;
@@ -38,8 +44,24 @@ public class Script_EnemyMovement : MonoBehaviour
             {
                 switchX = Vector2.left;
             }
-
         }
         rbody.velocity = switchX * moveSpeed;
+
+        if(enemyType == EnemyType.Melee)
+        {
+            RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position, switchX, 2f, ~EnemyMask);
+            if(hitPlayer.collider != null && hitPlayer.collider.CompareTag("Player"))
+            {
+                GameObject attackCollider = new GameObject("AttackCollider");
+
+                BoxCollider2D boxCollider = attackCollider.AddComponent<BoxCollider2D>();
+
+                attackCollider.transform.position = transform.position + new Vector3(switchX.x, switchX.y, 0);
+
+                boxCollider.size = new Vector2(1f, 1f);
+
+                Destroy(attackCollider, 0.5f);
+            }
+        }
     }
 }
