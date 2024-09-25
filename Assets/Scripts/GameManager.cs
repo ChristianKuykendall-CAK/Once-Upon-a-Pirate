24be2c411bb727570e5192acf211e2f591c37594
sh1ny11 <cjl35@pct.edu>
2024-09-25 17:55:14 -0400

    UI Updates Now.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    //UI Variables
     public int health = 100;
     public int ammo = 5;
     public int coins = 0;
+
+    //Text variables
+    public Text HealthText;
+    public Text AmmoText;
+    public Text CoinText;
 
     private void Awake()
     {
@@ -23,7 +30,13 @@ public class GameManager : MonoBehaviour
 
     void FixedUpdate()
     {
+        //Prevents player health from going above 100
         if(health > 100)
             health = 100;
+
+        //Updates the player's health, ammo, and coin count every frame
+        HealthText.text = "Health: " + health;
+        AmmoText.text = "Ammo: " + ammo;
+        CoinText.text = "Coins: " + coins;
     }
 }
