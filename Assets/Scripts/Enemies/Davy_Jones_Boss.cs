e18ffdada35d2553e9c7c5934c7814838d298bdd
Casper Whyne <cdw26@pct.edu>
2024-10-26 16:46:31 -0400

    Davy Chases

--- Assets/Scripts/Enemies/Davy_Jones_Boss.cs
+++ Assets/Scripts/Enemies/Davy_Jones_Boss.cs
@@ -5,18 +5,22 @@ using UnityEngine.AI;
 
 public class Davy_Jones_Script : MonoBehaviour
 {
-    public enum EnemyType { DavyJones };
-    public EnemyType enemyType;
+    //public enum EnemyType { DavyJones };
+    //public EnemyType enemyType;
 
-    public enum DavyState { Patrol, Chase, Attack };
+    //public enum DavyState { Patrol, Chase, Attack };
     public float chaseDistance = 10f;
     public float shootDistance = 2f;
     public float sliceDistance = 1f;
     public float recoverTime = 2.5f;
     public float speed = 10;
+    public float jumpForce = 3f;
+    
+    public LayerMask shipGround;
+    private bool isGrounded;
+    private bool shouldJump;
 
-    public DavyState davyState;
-
+    private float offset;
     private float distance;
     private int health = 500;
     private SpriteRenderer rend;
@@ -41,12 +45,12 @@ public class Davy_Jones_Script : MonoBehaviour
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody2D>();
         Vector2 switchX = Vector2.left;
-
-        //ChangeState(davyState);
     }
 
     void Update()
     {
+
+        //gets player transform and makes davy follow that
         distance = Vector2.Distance(playerTransform.position, transform.position);
         Vector2 direction = playerTransform.position - transform.position;
 
@@ -57,6 +61,7 @@ public class Davy_Jones_Script : MonoBehaviour
     private void FixedUpdate()
     {
 
+        //triggers the walking anim
         if (moveSpeed > 0)
         {
             anim.SetTrigger("isWalking");
@@ -64,6 +69,27 @@ public class Davy_Jones_Script : MonoBehaviour
             //anim.SetBool("isShooting", false);
         }
 
+        //if player is to the right, face right and vice versa
+        if (playerTransform.position.x > transform.position.x)
+        {
+            switchX = Vector2.right;
+            facingDirection = Vector2.right;
+            if (transform.localScale.x < 0) // If the sprite is flipped, flip it back
+            {
+                FlipX();
+            }
+        }
+        else if (playerTransform.position.x < transform.position.x)
+        {
+            switchX = Vector2.left;
+            facingDirection = Vector2.left;
+            if (transform.localScale.x > 0) // If the sprite is not flipped, flip it
+            {
+                FlipX();
+            }
+        }
+
+        //remove davy health if the player shoots him
         void OnCollisionEnter2D(Collision2D collision)
         {
             if (collision.gameObject.CompareTag("Bullet"))
@@ -72,6 +98,7 @@ public class Davy_Jones_Script : MonoBehaviour
             }
         }
 
+        //kills davy if health runs out
         if (health <= 0)
         {
             anim.SetTrigger("isDead");
@@ -80,118 +107,7 @@ public class Davy_Jones_Script : MonoBehaviour
 
     }
 
-    /*
-    void ChangeState(DavyState newState)
-    {
-        StopAllCoroutines();
-        davyState = newState;
-        switch (davyState)
-        {
-            case DavyState.Patrol:
-                StartCoroutine("AI_Patrol");
-                break;
-            case DavyState.Chase:
-                StartCoroutine("AI_Chase");
-                break;
-            case DavyState.Attack:
-                StartCoroutine("AI_Attack");
-                break;
-        }
-    }
-
-    IEnumerator AI_Patrol()
-    {
-        anim.SetBool("IsIdle", true);
-        anim.SetBool("IsWalking", false);
-
-        while (true) // keep the object patrolling 
-        {
-            // Are we close to the player -- if so, we need to chase
-            if (Vector2.Distance(playerTransform.position, transform.position) < chaseDistance)
-            {
-                ChangeState(EnemyState.Chase);
-                yield break;
-            }
-            else // Keep patrolling
-            {
-                Vector3 randomPosition = Random.insideUnitSphere * chaseDistance;
-                randomPosition += transform.position;
-
-                yield return new WaitForSeconds(3f);
-            }
-
-        }
-    }
-
-    IEnumerator AI_Chase()
-    {
-        anim.SetBool("IsWalking", true);
-        anim.SetBool("IsSlicing", false);
-        anim.SetBool("IsIdle", false);
-        billboard.enabled = true; //make face player
-
-        while (true) // keep the object patrolling 
-        {
-            //playerTransform = GameObject.FindGameObjectWithTag("player").transform;
-
-            // Are we close to the player -- if so, we need to chase
-            if (Vector3.Distance(playerTransform.position, transform.position) < attackDistance)
-            {
-                ChangeState(EnemyState.Attack);  //updated state here!
-                yield break;
-            }
-            else if (Vector3.Distance(playerTransform.position, transform.position) > chaseDistance)
-            {
-                ChangeState(EnemyState.Patrol);
-                yield break;
-            }
-            //playerTransform = GameObject.FindGameObjectWithTag("player").transform;
-            //agent.SetDestination(playerTransform.position); // the player's position
-            yield return new WaitForSeconds(1f);
-
-        }
-    }
-
-    IEnumerator AI_Attack()
-    {
-        anim.SetBool("IsAttacking", true);
-        anim.SetBool("IsChasing", false);
-        float elapsedTime = 3f;
-
-        while (true)
-        {
-            playerTransform = GameObject.FindGameObjectWithTag("player").transform;
-
-            // Are we close to the player -- if so, we need to chase
-            if (Vector3.Distance(playerTransform.position, transform.position) > attackDistance)
-            {
-                ChangeState(EnemyState.Chase);  //updated state here!
-                yield break;
-            }
-            //Debug.Log(elapsedTime);
-            elapsedTime += 1;
-            if (elapsedTime >= recoverTime)
-            {
-                //PlayerController player_controller = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerController>();
-                //player_controller.PlayerDamage(20);  // this is a bad way to do this; we'll update this later!
-                EventManager.TriggerEvent("PlayerDamage", 20);
-                Debug.Log("Damage player");
-                elapsedTime = 0f;
-            }
-
-            yield return new WaitForSeconds(1f);
-        }
-
-
-        
-        void Freeze()
-        {
-            frozen = false;
-            rbody.velocity = switchX * 0;
-        }
-        */
-
-        void OnTriggerEnter2D(Collider2D collider)
+    void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("EnemyAttack") ||
             collider.CompareTag("Ammo") ||
@@ -202,6 +118,13 @@ public class Davy_Jones_Script : MonoBehaviour
             return;
         }
         health -= 20;
+    }
+
+    void FlipX()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x = theScale.x * -1;
+        transform.localScale = theScale;
     }
 
     void Die()
