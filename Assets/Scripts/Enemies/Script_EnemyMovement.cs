46ab6c08cb8c2d6dbce77505699a630289ad2bb7
Casper Whyne <cdw26@pct.edu>
2024-11-07 10:52:06 -0500

    added skeleton and updated melee

--- Assets/Scripts/Enemies/Script_EnemyMovement.cs
+++ Assets/Scripts/Enemies/Script_EnemyMovement.cs
@@ -32,6 +32,7 @@ public class Script_EnemyMovement : MonoBehaviour
     private float offset;
     private bool frozen = false;
     private bool isDead = false;
+    private bool isSlicing = false;
 
     //Audio
     private AudioSource Audio;
@@ -112,10 +113,10 @@ public class Script_EnemyMovement : MonoBehaviour
             rbody.velocity = switchX * moveSpeed;
 
         //triggers the walking animation
-        if(moveSpeed > 0)
+        if(moveSpeed > 0 && isSlicing == false)
         {
             anim.SetTrigger("isWalking");
-            anim.SetBool("isSlicing", false);
+            //anim.SetBool("isSlicing", false);
         }
 
         if (enemyType == EnemyType.Melee)
@@ -164,6 +165,7 @@ public class Script_EnemyMovement : MonoBehaviour
 
                     Invoke("Freeze", 2f);
                     frozen = true;
+                    anim.ResetTrigger("isWalking");
 
                     EnemyattackCollider.transform.position = transform.position + new Vector3(switchX.x + offset, switchX.y, 0);
 
@@ -172,7 +174,13 @@ public class Script_EnemyMovement : MonoBehaviour
                     boxCollider.size = new Vector2(.5f, .5f);
 
                     Destroy(EnemyattackCollider, 0.5f);
+                    moveSpeed = 0;
                     anim.SetBool("isSlicing", true);
+                    
+                } else if (hitPlayer.collider == null)
+                {
+                    moveSpeed = 5;
+                    anim.SetBool("isSlicing", false);
                 }
             }
         }
