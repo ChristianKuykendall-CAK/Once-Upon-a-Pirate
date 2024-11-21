7c2c4072f59e84bed0361906923bf25c70bb480e
Christian Kuykendall <cak44@pct.edu>
2024-11-21 09:56:03 -0500

    level 2 update before merge

--- Assets/Scripts/BossArenaTrigger.cs
+++ Assets/Scripts/BossArenaTrigger.cs
@@ -8,16 +8,13 @@ public class BossArenaTrigger : MonoBehaviour
     public GameObject wall;
     public GameObject camera;
 
-    // Start is called before the first frame update
+    Vector3 cameraPosition;
+    public Transform cameraTransform;
+
     void Start()
     {
         wall.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        cameraPosition.Set(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -26,7 +23,7 @@ public class BossArenaTrigger : MonoBehaviour
         {
             wall.SetActive(true);
             camera.transform.SetParent(null);
-            camera.transform.position = new Vector3(698.5f, 21f, -1f);
+            camera.transform.position = cameraPosition;
             Camera.main.orthographicSize = 8;
         }
     }
